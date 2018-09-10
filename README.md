b432eb213c6b16657b1f8a541286b9375c1a6bb2
Aknotofknots <christianahlsen@gmail.com>
2018-09-10 11:55:54 +0200

    Project setup
    webpack setup for css and js files.
    babel-loader caches the transformed files locally to increase compile
    speed of webpack.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Gameified 3d portfolio using three.js, webpack and friends
+
+More updates during the upcoming weeks..
+
+..until then enjoy [this](https://www.youtube.com/watch?v=dXlao2KNYjQ) or smth :)
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        exclude: /(node_modules|bower_components)/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader?cacheDirectory=true',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: [require('@babel/plugin-proposal-object-rest-spread')]
+          }
+        }
+      }
+    ]
+  }
+};
